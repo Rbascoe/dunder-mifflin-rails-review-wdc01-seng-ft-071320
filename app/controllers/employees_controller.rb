cdfc0106cfed96b614a8d9bf13a449f1e6893d5f
Rbascoe <roanbascoe5@gmail.com>
2020-08-05 16:58:51 -0400

    close to completion

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,30 @@
 class EmployeesController < ApplicationController
+
+    def index
+        @employees = Employee.all
+    end
+
+    def show
+        id = params[:id]
+        @employee = Employee.find(id)
+    end
+
+    def new
+        @employee = Employee.new
+        @dogs = Dog.all
+    end
+
+    def create
+        first_name = params["employee"]["first_name"]
+        last_name = params["employee"]["last_name"]
+        alias_e = params["employee"]["alias"]
+        office = params["employee"]["office"]
+        title = params["employee"]["title"]
+        dog = params["employee"]["dog"]
+        img_url = params["employee"]["img_url"]
+        employee = Employee.create(first_name: first_name, last_name: last_name, alias: alias_e, office: office, title: title, dog_id: dog, img_url: img_url)
+        redirect_to employee_path(employee)
+
+        
+    end
 end
